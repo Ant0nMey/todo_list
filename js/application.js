7d50ac3416a0ddf7de6b02923707257a6ccdbf7c
Ant0n <root@UOP-0040-INFO.challauch.local>
2026-02-03 09:01:19 +0100

    ok

--- js/application.js
+++ js/application.js
@@ -34,6 +34,38 @@ class Observable{
     }
 }
 
+class ToDoModel{
+
+    /**
+     * @param {id: Number, title: String, description: String}
+     * @return void
+     */
+    constructor(obj) {
+        this.updateProperties(obj);
+    }
+
+    /**
+     * Map properties to this instance
+     * 
+     * @param {id: Number, title: String, description: String}
+     * @return void
+     */
+    updateProperties(obj) {
+        this.id = obj.id;
+        if (obj.title) this.title = obj.title;
+        if (obj.description) this.description = obj.description;
+    }
+
+    /**
+    * Get a list of properties for this class
+     * 
+    * @return {string[]}
+     */
+    static getFields() {
+    return ['id', 'title', 'description'];
+    }
+}
+
 class ProjectModel {
 
     /**
@@ -121,35 +153,170 @@ class ProjectsCollectionModel extends Observable{
         this.notifySubscriber();
     }
 
+    getToDo(id) {
+        let activeProject = this.getActiveProject();
+        return activeProject.toDos.find( it => it.id === id);
+    }
+
+    updateToDo(obj) {
+        console.log('obj :', obj);
+        const tudo = this.getToDo(obj.id);
+        tudo.updateProperties(obj);
+        this.notifySubscriber();
+    }
+
+    removeToDo(id) {
+        const project = this.getActiveProject();
+        project.toDos = project.toDos.filter(todo => todo.id !== id);
+        this.notifySubscriber();
+    }
 }
 
-class ToDoModel{
+/**
+ * GreyModalElement is a singleton
+ */
+const GreyModalElement = function() {
+    const element = document.getElementById('grey-modal-background');
+    return {
 
-    /**
-     * @param {id: Number, title: String, description: String}
-     * @return void
-     */
-    constructor(obj) {
+        /**
+         * Show the modal.
+         * 
+         * @return void
+         */
+        show: function() {
+            element.classList.remove('hidden');
+        },
+
+        /**
+         * Hide the modal and clear its content.
+         * 
+         * @return void
+         */
+        hide: function() {
+            element.innerHTML = "";
+            element.classList.add('hidden');
+        },
+
+        /**
+         * Append child element to the modal.
+         * 
+         * @return void
+         */
+        appendChild: function(childElement) {
+            element.appendChild(childElement);
+        }
+    }
+}();
+
+/**
+ * Project component. We call this a component as its behaviour is a
+ * reusable component for web composition.
+ * 
+ * With this design it is also easier to map it over to a true web-component,
+ * which will hopefully soon become a standard in all the major browsers.
+ */
+/*class ProjectComponent {
+
+    constructor(obj){
+        this.containerElement = obj.containerElement;
+        this.fileds = ProjectModel.getFields();
         this.updateProperties(obj);
+
+        this.showCreateProjectModalFn = () => {
+            const createProjectForm =
+                new CreateProjectFormComponent({}, this.projectsCollection)
+            createProjectForm.render();
+
+            GreyModalElement.appendChild(createProjectForm.formElement);
+            GreyModalElement.show();
+            createProjectForm.inputFieldForTitle.focus();
+        };
+
+        this.showEditProjectModalFn = event => {
+            const projectId =
+                event.target.getAttribute("data-project-id");
+
+        const editProjectForm =
+            new EditProjectFormComponent(
+                {},
+                this.projectsCollection,
+                this.projectsCollection.getProject(projectId)
+            );
+            editProjectForm.render()
+        }
+    }
+}
+*/
+
+class BaseFormAbstract{
+
+    constructor(obj){
+        if (new.target === BaseFormAbstract) {
+            throw new TypeError("Cannot construct Abstract instances directly");
+        }
+        this.method = obj.method || 'POST';
+        this.action = obj.action || null;
+        this.enctype = obj.enctype || null;
+
+        this.buildDOMElements();
+        this.updateAttributes();
     }
 
-    /**
-     * Map properties to this instance
-     * 
-     * @param {id: Number, title: String, description: String}
-     * @return void
-     */
-    updateProperties(obj) {
-        this.id = obj.id;
-        if (obj.title) this.title = obj.title;
-        if (obj.description) this.description = obj.description;
+    buildDOMElements() {
+        this.formElement = document.createElement('FORM');
+
+        this.submitButtonElement = document.createElement('BUTTON');
+        this.submitButtonElement.type = "submit";
+        this.submitButtonElement.textContent = 'Submit';
     }
-    /**
-    * Get a list of properties for this class
-     * 
-    * @return {string[]}
-     */
-    static getFields() {
-    return ['id', 'title', 'description'];
+
+    updateAttributes() {
+        this.formElement.method = this.method;
+        this.formElement.action = this.action;
+        if(this.enctype){
+            this.formElement.enctype = this.enctype;
+        }
+    }
+
+    submit(){
+        this.formElement.submit();
+    }
+}
+
+/**
+ * Abstract class ProjectFormAbstract
+ * This class contains business logic for the project form.
+ */
+class ProjectFormAbstract extends BaseFormAbstract {
+
+    constructor(obj){
+        super(obj);
+        if (new.target === ProjectFormAbstract) {
+            throw new TypeError("Cannot construct Abstract instances directly");
+        }
+
+        this.destroyFormFn = () => {
+            GreyModalElement.hide()
+        };
+
+        this.submitEventFn = event => {
+            event.preventDefault();
+            this.submit();
+        };
+
+        this.updateFormElement();
+        this.updateSubmitButtonElement();
+        this.buildCancelButton();
+    }
+
+    updateFormElement() {
+        this.formElement.className = "modal";
+    }
+
+    updateSubmitButtonElement() {
+        this.submitButtonElement.textContent = "Ajouter projet";
+        this.submitButtonElement.classList.add('green');
+        this.submitButtonElement.addEventListener('click', this.submitEventFn);
     }
 }
